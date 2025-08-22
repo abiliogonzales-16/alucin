a08d2b49acb3a9f193e5c0ab9cf78ac8dfd2a428
Abilio.12 <estudiantev@COED.LOCAL>
2025-08-22 01:04:35 -0600

    creacion de la base de datos

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Adios mundo")
+from sqlite3 import *
+
+
+baseDeDatos = connect("ALUCIN.db")
+cr = baseDeDatos.cursor()
+
+
+cr.execute('''
+CREATE TABLE IF NOT EXISTS palabras (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    palabra TEXT NOT NULL,
+    descripcion TEXT NOT NULL)''')
+
+baseDeDatos.commit()
+baseDeDatos.close()
