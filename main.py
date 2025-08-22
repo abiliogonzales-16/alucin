fcb1af88fa675356b3b05f1f6e8f7d39851d641d
Manuel <estudiantev@COED.LOCAL>
2025-08-22 01:09:16 -0600

    Hice un cambio leve

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
-print("Hola mundo")
+from tkinter import  *
+
+def juggar():
+    ventana = Toplevel(app)
+    etiqueta = Label(ventana,text="kfjhskfjh")
+    ventana.title("ventana emergente")
+    etiqueta.pack()
+
+
+# no esta en uso
+
+def salir ():
+    sali = ventana.destroy
+
+
+
+'''
+def calculate():
+    resul = eval(caja1.get())
+    caja1.delete(0, END)
+    caja1.insert(END, str(resul))
+
+otro.pop(actual)
+            cuaderno.forget(actual)
+'''
+
+
+app = Tk ()
+
+app.title("Ahogado")
+
+ventana = Tk
+
+btn = Button(app,text="jugar ",command=juggar)
+btn1 = Button(app,text="salir ", command=salir )
+
+btn1.pack()
+btn.pack()
+app.mainloop()
